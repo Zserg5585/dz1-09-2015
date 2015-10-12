91447ff26a10e9a0780b1cc17cca203390e50cd4
Zserg5585 <serg5585@gmail.com>
2015-10-13 02:44:13 +0300

    add popup and fix small bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,23 @@
-$(function() {
-    console.log('я на главной странице');
-    console.log($);
+$(document).ready(function(){
+
+    $('.add_pj').on('click', function(e) {
+        e.preventDefault();
+
+        $('.popup').bPopup({
+            closeClass : 'popup__close'
+        });
+    });
+
+    $('.form__input-file-origin').on('change', function(){
+
+        var
+            $this = $(this),
+            value = $this.val(),
+            pureVal = value.replace(/c:\\fakepath\\/gmi, "");
+
+        $('.form__input-fake').text(pureVal);
+
+    });
+
+
 });
